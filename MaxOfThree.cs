e50eb0019a168ad11fb36569afccef191c241e27
Sanjay-312 <sanjeevabodagalla@gmail.com>
2023-09-08 06:58:50 +0530

    [sanjeeva]: chekced all 3 test cases by giving max_values at 1st 2nd 3rd position

---
+++ MaxOfThree.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsAssignment
+{
+    internal class MaxOfThree
+    {
+        public static int get_max(int a,int b,int c)
+        {
+            if (a.CompareTo(b) > 0 && a.CompareTo(c) > 0 ||
+                a.CompareTo(b) >= 0 && a.CompareTo(c) > 0 ||
+                a.CompareTo(b) > 0 && a.CompareTo(c) >= 0)
+            {
+                return a;
+            }
+            if (b.CompareTo(a) > 0 && b.CompareTo(c) > 0 ||
+                b.CompareTo(a) >= 0 && b.CompareTo(c) > 0 ||
+                b.CompareTo(a) > 0 && b.CompareTo(c) >= 0)
+            {
+                return b;
+            }
+            if (c.CompareTo(a) > 0 && c.CompareTo(b) > 0 ||
+                c.CompareTo(a) >= 0 && c.CompareTo(b) > 0 ||
+                c.CompareTo(a) > 0 && c.CompareTo(b) >= 0)
+            {
+                return c;
+            }
+            throw new Exception("a, b, c are same");
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GenericsAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int max_value= MaxOfThree.get_max(32,15,18);
+            Console.WriteLine(max_value);
+        }
+    }
+}
